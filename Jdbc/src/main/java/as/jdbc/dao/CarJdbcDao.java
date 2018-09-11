fb21f243e8533b8d107e8a8ae23867b7d1982640
Adam Stankowski <stankowskia@gmail.com>
2018-09-11 19:58:43 +0200

    Adding Car entity calss and CarJdbcDao

---
+++ Jdbc/src/main/java/as/jdbc/dao/CarJdbcDao.java
@@ -0,0 +1,5 @@
+package as.jdbc.dao;
+
+public interface CarJdbcDao {
+    Car getById(Long id);
+}
---
+++ Jdbc/src/main/java/as/jdbc/model/Car.java
@@ -0,0 +1,42 @@
+package as.jdbc.model;
+
+import java.util.Date;
+
+public class Car {
+    private Long id;
+    private String make;
+    private String model;
+    private Date manufacturedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Date getManufacturedAt() {
+        return manufacturedAt;
+    }
+
+    public void setManufacturedAt(Date manufacturedAt) {
+        this.manufacturedAt = manufacturedAt;
+    }
+}
