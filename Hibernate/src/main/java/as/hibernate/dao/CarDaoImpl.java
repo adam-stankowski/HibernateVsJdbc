66c02273fd324e7964a4a584ffcb6a495a5b7e66
Adam Stankowski <stankowskia@gmail.com>
2018-09-11 12:23:50 +0200

    Adding Passing CarDaoImpl code

--- Hibernate/src/main/java/as/hibernate/dao/CarDaoImpl.java
+++ Hibernate/src/main/java/as/hibernate/dao/CarDaoImpl.java
@@ -2,10 +2,29 @@ package as.hibernate.dao;
 
 import as.hibernate.model.Car;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class CarDaoImpl implements CarDao {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hibernatePlayground");
+
+    private static EntityManager createEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
 
     @Override
-    public Car getById(Long id) {
-        return null;
+    public Car getById(Long id){
+        EntityManager entityManager = createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Car car = entityManager.find(Car.class, id);
+
+        transaction.commit();
+        entityManager.close();
+
+        return car;
     }
 }
